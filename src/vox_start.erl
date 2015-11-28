b9c40e628bd11a27ef30660487a2a6a0bd18b6d6
Andrii Melnykov <andy.melnikov@gmail.com>
2015-11-28 17:49:03 +0200

    Hard-coded chroot

--- src/vox_start.erl
+++ src/vox_start.erl
@@ -5,13 +5,14 @@ command(Args) -> {ok,lists:map(fun start/1,Args)}.
 
 start(App) ->
     vox:info("App: ~p~n",[App]),
-    {ok,Bin}  = file:read_file(lists:concat(["apps/",App,"/config.json"])),
+    Dir       = filename:join(["apps", App]),
+    {ok,Bin}  = file:read_file(filename:join([Dir, "config.json"])),
     {Json   } = jsone:decode(Bin),
     {Process} = proplists:get_value(<<"process">>,Json),
     Args      = proplists:get_value(<<"args">>,Process),
-    Concat    = string:join(lists:map(fun(X) -> binary_to_list(X) end,Args)," "),
-    {_,R,S}   = sh:run(Concat,<<"log">>,lists:concat(["apps/",App])),
+    Concat    = ["chroot","--userspec=99:99", "rootfs"] ++ lists:map(fun(X) -> binary_to_list(X) end,Args),
     vox:info("Oneliner: ~p~n",[Concat]),
+    {_,R,S}   = sh:run(Concat,<<"log">>,Dir),
     {ret(R),S}.
 
 ret(0) -> ok;
