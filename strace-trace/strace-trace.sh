06d55280b6099bf574f67ce28ea84c1eff5944b0
Andrii Melnykov <andy.melnikov@gmail.com>
2016-03-01 01:24:25 +0200

    New: strace child processes and more

--- strace-trace/strace-trace.sh
+++ strace-trace/strace-trace.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-	strace -e 'trace=open' -o strace.log $*
-	cat strace.log | grep -Ev '(ENOENT|^(\+\+\+|---))' | cut -d '"' -f 2 |  grep -vE '^/(dev)/|^/etc/ldd.cache$' | sort | uniq >spec
+	strace -e 'trace=open,execve' -f -q -o strace.log $*
+	cat strace.log | grep -Ev '(ENOENT|^(\+\+\+|---)|(\+\+\+|---)$)' | cut -d '"' -f 2 |  grep -vE '^/(dev|sys|run|tmp)/|^/etc/ld.so.cache$' | sort | uniq >spec
 
