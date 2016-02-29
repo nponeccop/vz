b0f323ef2c0c05843f5fb790df69c0c11a9ad605
Andrii Melnykov <andy.melnikov@gmail.com>
2016-02-29 22:13:55 +0200

    New: ldd-trace.sh to copy C libraries

---
+++ ldd-trace/ldd-chroot.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Author: https://tetsumaki.net/blog/article/2013-06-11-creation-dun-environnement-chroot-arch-linux.html
+
+# Utilisation: $0 <chroot_path> <program list>
+rep="$1"
+shift
+
+# Copie $1 vers $2 en créant les répertoires parents.
+copie_dir ()
+{
+	[ -e "${2}" ] && return
+ 	rep_base=$(dirname "${2}")
+ 	[ -d "${rep_base}" ] || {
+ 		echo "++ mkdir -p ${rep_base}"
+ 		mkdir -p "${rep_base}"
+ 	}
+ 	echo "+ cp -a $1 $2"
+ 	cp -a "$1" "$2"
+}
+ 
+# Copie $1 vers $2 + copie des bibliothèques utilisées.
+copie_ldd ()
+{
+	local src dest file f f_link
+ 	src="$1"
+ 	dest="$2"
+ 	[ -e "${dest}" ] && return
+ 	file=( $(ldd "$src" | awk '{print $3}' | grep '^/') )
+ 	file=( "${file[@]}" $(ldd "$src" | grep '/' | grep -v '=>' | awk '{print $1}') )
+ 	for f in "${file[@]}"
+ 	do
+ 		f_link=$(readlink -f "$f")
+ 		copie_dir "$f_link" "${rep}${f}"
+ 	done
+ 	copie_dir "$src" "${dest}"
+}
+ 
+for prog in "$@"
+do
+ 	prog=$(which "$prog")
+ 	prog_real=$(readlink -f "$prog")
+ 	copie_ldd "$prog_real" "${rep}${prog}"	
+done
+
