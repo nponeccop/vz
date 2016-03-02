046865bb2cd7a2c49f8875a5315618ca6d7e0c06
nponeccop <andy.melnikov@gmail.com>
2016-03-02 16:23:56 -0400

    Fix: a partial fix for broken symlinks

--- strace-trace/strace-trace.sh
+++ strace-trace/strace-trace.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-	strace -e 'trace=open,execve' -f -q -o strace.log $*
-	cat strace.log | grep -Ev '(ENOENT|^(\+\+\+|---)|(\+\+\+|---)$)' | cut -d '"' -f 2 |  grep -vE '^/(dev|sys|run|tmp)/|^/etc/ld.so.cache$' | sort | uniq >spec
-
+set -ex
+strace -e 'trace=open,execve' -f -q -o strace.log $*
+cat strace.log | grep -Ev '(ENOENT|^(\+\+\+|---)|(\+\+\+|---)$)' | cut -d '"' -f 2 |  grep -vE '^/(dev|sys|run|tmp)/|^/etc/ld.so.cache$' | sort | uniq >spec
+join -v1 <(cat spec | xargs readlink -m | sort) spec >>spec
