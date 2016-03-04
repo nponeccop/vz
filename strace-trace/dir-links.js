0d4e80bc7d91a48aa7a4feaad86c9db81426014b
Andrii Melnykov <andy.melnikov@gmail.com>
2016-03-04 03:42:34 +0200

    New: shebang tracing

--- strace-trace/dir-links.js
+++ strace-trace/dir-links.js
@@ -2,16 +2,17 @@ var fs = require('fs')
 var path = require('path')
 var f = fs.readFileSync(process.argv[2], {encoding:'utf-8'}).split('\n')
 var assert = require('assert')
+var shebang = require('./shebang')
 f.pop()
 
 var l = {}
 
 var SRC = '/'
 
-f.forEach(function (src)
+function resolve(src)
 {
 	var d = path.relative(SRC, src)
-	console.log(path.resolve(SRC, d.split('/').reduce(function (p, comp)
+	var p = path.resolve(SRC, d.split('/').reduce(function (p, comp)
 	{
 		assert.notEqual(comp, '')
 		var np = p + '/' + comp
@@ -25,5 +26,13 @@ f.forEach(function (src)
 		{
 			return np
 		}
-	}, '')))
-})
+	}, ''))
+	console.log(p)
+	var sb = shebang.check(p)
+	if (sb != null)
+	{
+		resolve(sb)
+	}
+}
+
+f.forEach(resolve)
---
+++ strace-trace/shebang.js
@@ -0,0 +1,36 @@
+var fs = require('fs')
+
+var she = '#'.codePointAt(0)
+var bang = '!'.codePointAt(0)
+var slash = '/'.codePointAt(0)
+var lf = '\n'.codePointAt(0)
+var space = ' '.codePointAt(0)
+
+exports.check = function (fname)
+{
+	if (!fs.lstatSync(fname).isFile())
+	{
+		return null
+	}
+	var h = fs.openSync(fname, 0) 
+	if (!h)
+	{
+		return null
+	}
+	var b = new Buffer(1024)
+	b[0] = 0
+	fs.readSync(h, b, 0, b.length)
+	if (b[0] == she && b[1] == bang && b[2] == slash)
+	{
+		for (var i = 0; i < b.length; i++)
+		{
+			if (b[i] == space || b[i] == lf) break
+		}
+		
+		return i < b.length ? b.slice(2, i).toString() : null
+	}
+	else
+	{
+		return null
+	}
+}
