867fd79631d5c713547bb3bbbb62bbeb3b36c149
Andrii Melnykov <andy.melnikov@gmail.com>
2016-03-09 02:24:56 +0200

    New: multiple improvements of strace-trace

--- master/vzbuild/strace-trace.sh
+++ master/vzbuild/strace-trace.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 set -ex
+
+DIR=$(dirname $0)
 OUT=$1
 shift
 strace -e 'trace=open,execve' -f -q -o strace.log $* || true
 
-node ./dir-links.js <(
+function grep_nosys {
+	grep -vE '^/(dev|sys|run|tmp|proc)/|^(/etc/ld.so.cache|/|/var/cache/ldconfig/aux-cache)$';
+}
 
-(cat strace.log | grep -E '^[0-9]+  [a-z]+\(' | grep -Ev 'ENOENT' | cut -d '"' -f 2 |  grep -vE '^/(dev|sys|run|tmp|proc)/|^(/etc/ld.so.cache|/)$';
+(
+node $DIR/dir-links.js <(
+
+	cat strace.log | grep -E '^[0-9]+  [a-z]+\(' | grep -Ev 'ENOENT' | cut -d '"' -f 2 | grep_nosys | sort -u
+	echo /usr/lib/ld-linux.so.2
+) | grep_nosys
+
 cat <<bar
 /run
 /dev
@@ -15,8 +25,5 @@ cat <<bar
 /proc
 /etc/resolv.conf
 /lib
-/usr/lib/ld-linux.so.2
 bar
-
-) | sort -u
 ) | sort -u -o $OUT
