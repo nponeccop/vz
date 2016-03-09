fdc2df7dbeb88ab0fed63dfe78a2ad50cc56a504
Andrii Melnykov <andy.melnikov@gmail.com>
2016-03-09 19:33:36 +0200

    New: PoC runch start

--- slave/runch/runch.sh
+++ slave/runch/runch.sh
@@ -1,16 +1,43 @@
 #!/usr/bin/env bash
-set -ex
-bundle=$1
-uu=$(jshon -F $bundle/config.json -e root -e path -u -pp -e process -e args -au -pp -e user -e uid -u -p -e gid -u -pp -e cwd -u |
-(
-read root
-read args
-read uid
-read gid
-read cwd
-cd $bundle$cwd 
-echo $uid:$gid $bundle/$root $args
-)
-)
+function cmd_start 
+{
+	local bundle=$1
+	if [ -z "$bundle" ]
+	then
+		echo "list: bundle cannot be empty" >&2
+		exit
+	fi
 
-chroot --userspec $uu
+	if [ -f $bundle/config.json ]
+	then
+		true
+	else
+		echo "$bundle/config.json doesn't exist" >&2
+		exit
+	fi
+	local uu=$(jshon -F $bundle/config.json -e root -e path -u -pp -e process -e args -au -pp -e user -e uid -u -p -e gid -u -pp -e cwd -u |
+	(
+	read root
+	read args
+	read uid
+	read gid
+	read cwd
+	cd $bundle$cwd 
+	echo $uid:$gid $bundle/$root $args
+	)
+	)
+
+	echo arch-chroot --userspec $uu
+}
+
+set -e
+case $1 in
+    start)
+      cmd_start $2
+      ;;
+	*)
+      echo "Invalid command: -$1" >&2
+	  exit
+      ;;
+esac
+
