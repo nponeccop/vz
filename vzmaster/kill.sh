b5eae0be7b1a1f1e2719f496b04fb79bdb8232ef
Andrii Melnykov <andy.melnikov@gmail.com>
2016-04-10 02:51:16 +0300

    New: a prototype vzmaster kill

--- vzmaster/kill.sh
+++ vzmaster/kill.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 set -ex -o pipefail
-pgid=$(ps -o pgid= $(cat /run/opencontainer/chroots/$1/state.json | jshon -e init_process_pid -u) | awk '{print $1}')
-
+container_id=$1
+pid=$(cat /run/opencontainer/chroots/$container_id/state.json | jshon -e init_process_pid -u)
+pgid=$(ps -o pgid="" $pid | awk '{print $1}')
 sudo kill -- -$pgid
+
+echo waiting for $pid to exit
+while [ -d /proc/$pid ] ; do sleep 1; done
+echo process $pid exited
+$(dirname $0)/runch delete $container_id
