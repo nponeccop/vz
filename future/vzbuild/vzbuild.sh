8a23e916d1e39530275dd02cbca29cbad14e29ce
Andrii Melnykov <andy.melnikov@gmail.com>
2016-04-25 05:16:05 +0300

    Internal: vzbuild itself was missing
    For #45

---
+++ future/vzbuild/vzbuild.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+function cmd_update
+{
+	set -x
+	local base=$1
+	./strace-parse.sh $1.trace >$1.parsed
+	sudo ./strace-spec.sh $1.parsed >$1.spec
+	sudo rm -r $1 || true
+	./from-spec.sh $1
+	sudo rm -rf ../../oci_bundles/$1/rootfs
+	mv $1/rootfs ../../oci_bundles/$1
+	rmdir $1
+	./from-bundle.sh ../../oci_bundles/$1
+	local image=$1-*.txz
+	if [-f ../../vzbuild/images/$image ]
+	then
+		echo $image already exists
+		rm $image
+		return
+	fi
+	echo $image
+	mv $image ../../vzmaster/images
+}	
+
+set -e -o pipefail
+case $1 in
+   update|foo)
+      cmd_$1 $2
+      ;;
+	*)
+      echo "Invalid command: $1" >&2
+	  exit
+      ;;
+esac
