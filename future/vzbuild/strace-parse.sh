c934e3038a6a81e33886f9a505d2deab8da85ddf
Andrii Melnykov <andy.melnikov@gmail.com>
2017-08-23 01:58:14 +0300

    Fix: new format of strace
    `execve("/usr/bin/node", ["node", "/home/foo/bar"], 0xbfb79a74 /* 13 vars */) = 0`

--- future/vzbuild/strace-parse.sh
+++ future/vzbuild/strace-parse.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -ex -o pipefail
-cat $1 | grep -Ev '^[+\-]|= -1 E[A-Z]+ \([A-Z][A-Za-z ]+\)$' | sed -e 's|^open("\(.*\)", O_.* = [0-9]*$|\1|;s|^execve("\(.*\)", \[.*]) = [0-9]*|\1|' | sort -u
+cat $1 | grep -Ev '^[+\-]|= -1 E[A-Z]+ \([A-Z][A-Za-z ]+\)$' | sed -e 's|^open("\(.*\)", O_.* = [0-9]*$|\1|;s|^execve("\(.*\)", \[.*], 0x.* .*) = [0-9]*|\1|' | sort -u
