cb327863cd7c53067926dfe55804b2418ccc0f2d
Andrii Melnykov <andy.melnikov@gmail.com>
2018-06-13 20:50:34 +0300

    New: reconstruction of unifinished strace syscalls

--- future/vzbuild/strace/parse.js
+++ future/vzbuild/strace/parse.js
@@ -1,24 +1,72 @@
 const grammar = require('./grammar')
 
-
 const parse2 = grammar.parse2
+let unfinished = {}
 
-if (false)
-{
-	const f = require('fs').readFileSync(process.argv[2], 'utf8').split('\n')
-	const idx = 318
-	console.log(f[idx])
-	const r = parse2(f[idx])
-	console.log(r == null ? r : r.rest == '' ? r.value[1] : { value: r.value[1].execve, rest: r.rest})
-	f.forEach((x, idx) => {
-		const res = parse2(x)
-		if (res == null)
+const f = require('fs').readFileSync(process.argv[2], 'utf8').split('\n')
+f.filter(x => x != '').map(x => {
+	const [pid, o] = grammar.parse(x)
+	const suspend = (co) =>
+	{
+		const [kk] = Object.keys(co)
+		if (kk == 'unfinished')
 		{
-			console.log(idx)
-			console.log(x)
-	//		console.log(res.value[2][5])
-	//		console.log(res)
+			if (pid in unfinished)
+			{
+				throw new Error("double unfinished!")
+			}
+			unfinished[pid] = o
+			return true
 		}
-	})
-}
+		else
+		{
+			return false
+		}
+	}
+
+	const [k] = Object.keys(o)
+	const v = o[k]
+
+	switch (k)
+	{
+	case 'execve':
+		const [path, co] = v
+		suspend(co)
+		break;
+	case 'openat':
+		{
+			const co = v[5]
+			if (suspend(co))
+			{
+				return null
+			}
+		}
+	case 'exit':
+	case 'signal':
+		break;
+	case 'resumed':
+		const [call, ret] = v
+
+		if (('unfinished' in ret)) {
+			throw new Error('unfinished resume!')
+		}
+		const oo = unfinished[pid]
+//		console.log(oo)
+		const [kkk] = Object.keys(oo)
+		switch (kkk) {
+		case 'execve':
+			oo.execve[1] = ret
+			return [pid, oo]
+		case 'openat':
+			oo.openat[5] = ret
+			return [pid, oo]
+		}
+
+		break;
+	default:
+		console.log(o)
+		process.exit()
+	}
+	return [pid, o]
+}).filter(x => x != null).forEach(x => console.log(x))
 
